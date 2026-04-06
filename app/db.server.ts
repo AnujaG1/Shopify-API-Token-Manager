7f8d33a2cc5d9b49206e4a8e63f6250a46bd47f9
Anuja Gyawali <anujagyawali000@gmail.com>
2026-04-06 12:51:07 +0545

    prisma edited

--- app/db.server.ts
+++ app/db.server.ts
@@ -8,6 +8,6 @@ export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["query","error", "warn"],
-  });
+  });ApiVersion.October25,
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
